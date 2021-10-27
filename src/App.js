7196b433e80bbc075c8ca00672cee036af1747c7
Amy Shackles <amyshackles@gmail.com>
2021-10-27 07:45:45 -0700

    Add site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <h1 className="miss-you">We're going to miss you, Ashley!</h1>
+            <iframe
+                src="https://app.mural.co/embed/6764ec47-fafb-461c-bb54-99ed654cdc0e"
+                width="640px"
+                height="480px"
+                style={{
+                    minWidth: "640px",
+                    minHeight: "480px",
+                    backgroundColor: "#f4f4f4",
+                    border: "1px solid #efefef",
+                }}
+                sandbox="allow-same-origin allow-scripts allow-modals allow-popups allow-popups-to-escape-sandbox"
+            ></iframe>
+        </div>
+    );
 }
 
 export default App;
